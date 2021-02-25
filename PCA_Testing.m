83d31538f360adb65f07daf9e522030dbd5a35fe
Anouar BOUCHAL <bouchal.anouar@gmail.com>
2021-02-25 16:24:10 +0100

    Added the testing script

--- PCA_Testing.m
+++ PCA_Testing.m
@@ -1,19 +1,44 @@
 %MATLAB Program for Testing
-[filename, pathname] = uigetfile('*.*', 'Select the input Image');
+[filename, pathname] = uigetfile('*.*', 'Select an image : ');
+if isequal(filename, 0)
+   disp('User selected Cancel');
+else
+   disp(['User selected ', fullfile(pathname, filename)]);
+end
 filewithpath = stract(pathname, filename);
+
+% Reading the image
 img  = imread(filewithpath);
 imgo = img;
+
+% Converting image to gray scale
 img  = rgb2gray(img);
+
+% Resizing image
 img  = imresize(img, [M, N]);
+
+% Reshaping matrix image to vector
 img  = double(reshape(img, [1, M*N]));
-imgpca = (img - m)*Ppca; %Projecting query image to Pca space
-distarray = zeros(n, 1); %initialize difference array
+
+% Projecting the image to Pca space
+imgpca = (img - m)*Ppca;
+
+% initialize difference array
+distarray = zeros(n, 1);
+ 
+% Finding L1 distance
 for i = 1:n
-    distarray(i) = sum(abs(T(i, :) - imgpca)); %Finding L1 distance
+    distarray(i) = sum(abs(T(i, :) - imgpca));
 end
-[result, index] = min(distarray); %Getting best match
-resultimg = imgread(sprintf('%d.jpg', index)); %Getting best matched
-%Plotting Images
+
+% Getting best match
+[result, index] = min(distarray);
+disp(result);
+
+% Getting the corresponding image
+resultimg = imgread(sprintf('%d.jpg', index));
+
+% Plotting Images
 subplot(imgo);
 title('Query Face');
 subplot(122)
