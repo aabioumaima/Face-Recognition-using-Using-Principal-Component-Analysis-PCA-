fb1727fc99fe36d9d0a7bf1d070149585aa74d18
Anouar BOUCHAL <bouchal.anouar@gmail.com>
2021-02-25 16:35:10 +0100

    Added milestones

--- PCA_Training.m
+++ PCA_Training.m
@@ -8,7 +8,7 @@ L = input('Enter No. of Dominant Eigen Values to keep : ');
 X = zeros(n, (M*N)); % Intialize Dataset matrix [X]
 T = zeros(n, L); % Initilize Transformed data set [T] in PCA space
 
-disp('Reading images ...');
+disp('[1/8] Reading images ...');
 for count=1:n
     I = imread(strcat(TRAIN_DIR, int2str(count), '.jpg')); %Reading images
     I = rgb2gray(I);
@@ -16,29 +16,29 @@ for count=1:n
     X(count, :) = reshape(I, [1, M*N]); %Rehsaping images as 1D vector
 endfor
 
-disp('Calculating mean ...');
+disp('[2/8] Calculating mean ...');
 Xb = X; %Copy database for further use
 m  = mean(X); %Mean of all Images
 
-disp('Substracting mean ...')
+disp('[3/8] Substracting mean ...')
 for i = 1:n
     X(i,:) = X(i,:) - m; %substracting Mean from each 10 image
 endfor
 
-disp('Calculating Covariance Matrix ...');
+disp('[4/8] Calculating Covariance Matrix ...');
 Q = (X'*X)/(n - 1);   % Covaraince Matrix
 
-disp('Calculating eigne vectors and values');
+disp('[5/8] Calculating eigne vectors and values ...');
 [Evecm, Evalm] = eig(Q); % Getting eigen values and Eigen vectors of Cov Matrix
 
-disp('Sorting the values and the vectors ...');
+disp('[6/8] Sorting the values and the vectors ...');
 [Evalsorted, Index] = sort(Eval, 'descen'); % Sorting eigen values
 Evecsorted = Evecm(:, Index);
 
-disp('Getting the PCA Matrix by selecting the first %d Vecors ...', L);
+disp('[7/8] Getting the PCA Matrix by selecting the first %d Vecors ...', L);
 Ppca = Evecsorted(:, 1:L); % Reduced trsnformation matrix [Ppca]
 
-disp('Projecting the images into the PCA space (X-m)*RCA ...');
+disp('[8/8] Projecting the images into the PCA space (X-m)*RCA ...');
 for i = 1:n
     T(i, :) = (Xb(i, :) - m)*Ppca; % Projecting each image to PCA space
 end
